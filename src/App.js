03998fe174d4896b5790b8c8503c3098f8a71ebe
yban <laubana@gmail.com>
2023-05-29 15:37:11 -0700

    Button component edited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import { Button } from "./components/base/Button/Button";
 function App() {
   return (
     <div className="App">
-      <Button label="Submit" size="md" />
+      <Button label="Button" size="lg" primary={true} />
+      <Button label="Button" size="lg" />
+      <Button label="Button" size="md" primary={true} />
+      <Button label="Button" size="md" />
+      <Button label="Button" size="sm" primary={true} />
+      <Button label="Button" size="sm" />
+      <Button label="Button" size="lg-block" primary={true} />
+      <Button label="Button" size="lg-block" />
+      <Button label="Button" size="md-block" primary={true} />
+      <Button label="Button" size="md-block" />
+      <Button label="Button" size="sm-block" primary={true} />
+      <Button label="Button" size="sm-block" />
+      <Button label="Button" size="lg" backgroundColor="black" color="white" />
+      <Button label="Button" size="lg" backgroundColor="red" color="white" />
     </div>
   );
 }
--- src/components/base/Button/Button.jsx
+++ src/components/base/Button/Button.jsx
@@ -4,20 +4,32 @@ import "./button.css";
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
+export const Button = ({
+  primary,
+  size,
+  onClick,
+  backgroundColor,
+  color,
+  label,
+  ...props
+}) => {
   const mode = primary
     ? "storybook-button--primary"
     : "storybook-button--secondary";
 
-  const onClickHandler = () => console.log("I got click handler");
+  const style = {
+    backgroundColor: backgroundColor || null,
+    color: color || null,
+  };
+
   return (
     <button
       type="button"
       className={["storybook-button", `storybook-button--${size}`, mode].join(
         " "
       )}
-      onClick={onClickHandler}
-      style={backgroundColor && { backgroundColor }}
+      onClick={onClick}
+      style={style}
       {...props}
     >
       {label}
