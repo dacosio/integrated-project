926790f2815e90e3ec655f2f605722f357569762
Cylvia Lian <cyl@Cylvias-MacBook-Pro.local>
2023-05-29 16:21:41 -0700

    pagetabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button } from "./components/base/Button/Button";
+import { Pagetabs } from "./components/module/pagetabs/pagetabs";
+
 function App() {
   return (
     <div className="App">
       <Button label="Submit" size="md" />
+      <Pagetabs primary item1="tab1" item2="tab2" item3="tab3"/>
+      <Pagetabs item1="tab1" item2="tab2" item3="tab3"/>
     </div>
   );
 }
--- src/components/base/Button/Button.jsx
+++ src/components/base/Button/Button.jsx
@@ -13,9 +13,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
       onClick={onClickHandler}
       style={backgroundColor && { backgroundColor }}
       {...props}
