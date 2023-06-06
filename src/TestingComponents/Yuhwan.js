bd82215cb1f7e64ea7016906db209b8e1753707a
yban <laubana@gmail.com>
2023-06-05 20:30:11 -0700

    feature/date-picker

--- src/TestingComponents/Yuhwan.js
+++ src/TestingComponents/Yuhwan.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import DatePicker from "../components/base/DatePicker/DatePicker";
 
 const Yuhwan = (props) => {
-  const [first, setfirst] = useState();
+  const [first, setfirst] = useState("");
 
   return (
     <div>
       <h1>Yuhwan</h1>
       <div>Test here</div>
+      <DatePicker date={first} setDate={setfirst}></DatePicker>
+      <div>{first}</div>
     </div>
   );
 };
