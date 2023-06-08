316b7710b99ee23a497619b6c0d58939caf566a7
Cylvia Lian <cyl@Cylvias-MacBook-Pro.local>
2023-06-08 12:37:15 -0700

    inprogress transaction card

--- src/TestingComponents/Cylvia.js
+++ src/TestingComponents/Cylvia.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import TransactionCard from "../components/base/TransactionCard/TransactionCard";
 
 const Cylvia = (props) => {
   const [first, setfirst] = useState();
   return (
     <div>
       <h1>Cylvia</h1>
-      <div>Test here</div>
+      <TransactionCard type="selling" itemName="banana" time="1" portions="3" sellerName="jsx" price="10"/>
     </div>
   );
 };
---
+++ src/components/base/TransactionCard/TransactionCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Button from "../Button/Button";
+import "./TransactionCard.css";
+
+const TransactionCard = (props) => {
+  const { type, itemName, time, portions, sellerName, price } = props;
+
+  const cardStyle = {
+      border: type === 'buying' ? '#0B5CBB 2px solid' : '#FFBF44 2px solid',
+    };
+
+  const buttons = () => {
+    if (type === 'buying') {
+      return (
+        <Button label="pending" />
+      )
+    } else {
+        return (
+          <>
+            <Button label="decline" />
+            <Button label="accept" />
+          </>
+        )
+      }
+    }
+
+  return (
+    <div className={["transaction-card", { type }].join(" ")} style={cardStyle}>
+      <div className="image-container"></div>
+      <div className="product-information">
+        <div>
+          <h3>{itemName}</h3>
+          <p className="price">${price}</p>
+        </div>
+        <p className="time">{time} days ago</p>
+        <p className="quantity">
+          <strong>Quantity:</strong> {portions}
+        </p>
+        <div className="seller-container">
+          <p>
+            <strong>Seller:</strong>
+          </p>
+          <p>{sellerName}</p>
+        </div>
+      </div>
+      <div className="button-container">
+        {buttons()}
+        {/* {type === 'buying' ? <Button label="pending" /> : <><Button label="Accept" /><Button label="Cancel"/></>} */}
+      </div>
+    </div>
+  );
+};
+
+export default TransactionCard;
