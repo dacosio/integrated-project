77f6531e7bacb385c34db5fe1ef551727f44bcde
Don Apollo Cosio <dacosio@gmail.com>
2023-06-08 14:50:05 -0700

    fix

--- src/components/base/ImageLabel/imageLabel.jsx
+++ src/components/base/ImageLabel/imageLabel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./ImageLabel.css";
+import "./imageLabel.css";
 
 export const ImageLabel = ({
   distance,
@@ -8,15 +8,10 @@ export const ImageLabel = ({
   color,
   ...props
 }) => {
-  const style = {
-    backgroundColor: backgroundColor || null,
-    color: color || null,
-  };
-
   const p1style = {
     borderRight: `#0b5cbb 1px solid`,
     fontWeight: `bold`,
-  }
+  };
 
   let p1;
   let p2;
@@ -37,12 +32,12 @@ export const ImageLabel = ({
     }
   }
 
-    return (
-      <div className="image-label">
-        {p1}
-        {p2}
-      </div>
-    );
-  };
+  return (
+    <div className="image-label">
+      {p1}
+      {p2}
+    </div>
+  );
+};
 
 export default React.memo(ImageLabel);
