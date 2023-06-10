b7be1e2044c9020593e81d50fd3627621508aadb
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-10 13:32:08 -0700

    feature/typography

--- src/TestingComponents/Yuki.js
+++ src/TestingComponents/Yuki.js
@@ -1,60 +1,21 @@
 import React from "react";
 import Typography from "../components/base/Typography/Typography";
 
-const typographyStyles = {
-  h1: {
-    fontSize: "40px",
-    fontFamily: "Graphik Bold",
-  },
-  h2: {
-    fontSize: "32px",
-    fontFamily: "Graphik Bold",
-  },
-  h3: {
-    fontSize: "28px",
-    fontFamily: "Graphik Bold",
-  },
-  h4: {
-    fontSize: "16px",
-    fontFamily: "Graphik Bold",
-  },
-  body1: {
-    fontSize: "16px",
-    fontFamily: "Graphik Medium",
-  },
-  body2: {
-    fontSize: "16px",
-    fontFamily: "Graphik Regular",
-  },
-  body3: {
-    fontSize: "12px",
-    fontFamily: "Graphil Medium",
-  },
-  body4: {
-    fontSize: "12px",
-    fontFamily: "Graphik Regular",
-  },
-  button: {
-    fontSize: "16px",
-    fontFamily: "Graphik Semiold",
-  },
-};
-
 const Yuki = () => {
   return (
     <div>
       <section>
         <h1 style={{ textDecoration: "underline" }}>Yuki</h1>
         <h3>Typography</h3>
-        <div style={typographyStyles.h1}>heading 1</div>
-        <div style={typographyStyles.h2}>heading 2</div>
-        <div style={typographyStyles.h3}>heading 3</div>
-        <div style={typographyStyles.h4}>heading 4</div>
-        <div style={typographyStyles.body1}>Body 1</div>
-        <div style={typographyStyles.body2}>Body 2</div>
-        <div style={typographyStyles.body3}>Body 3</div>
-        <div style={typographyStyles.body4}>Body 4</div>
-        <div style={typographyStyles.button}>Button</div>
+        <Typography className="h1-graphik-bold">h1-graphik-bold</Typography>
+        <Typography className="h2-graphik-bold">h2-graphik-bold</Typography>
+        <Typography className="h3-graphik-bold">h3-graphik-bold</Typography>
+        <Typography className="h4-graphik-bold">h4-graphik-bold</Typography>
+        <Typography className="body-1-regular">body-1-regular</Typography>
+        <Typography className="body-2-regular">body-2-regular</Typography>
+        <Typography className="body-3-regular">body-3-regular</Typography>
+        <Typography className="body-4-regular">body-4-regular</Typography>
+        <Typography className="button-regular">button-regular</Typography>
         <hr />
       </section>
     </div>
