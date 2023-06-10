31384aae77113dbce8a7e06a08c8cb1a1f497870
Don Apollo Cosio <dacosio@gmail.com>
2023-06-10 12:34:33 -0700

    initial commit

--- src/TestingComponents/Don.js
+++ src/TestingComponents/Don.js
@@ -2,19 +2,38 @@ import React, { useState } from "react";
 import FirebaseSample from "../config/FirebaseSample";
 import ImageUpload from "../components/base/ImageUpload/ImageUpload";
 import SearchField from "../components/base/SearchField/SearchField";
+import MapLeaflet from "../components/module/MapLeaflet/MapLeaflet";
+import Autocomplete from "react-google-autocomplete";
+import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
+import { useEffect } from "react";
+import MapSearch from "../components/base/MapSearch/MapSearch";
+
+const wrapper = {
+  padding: "1rem",
+};
 
 const Don = (props) => {
+  const mapCenter = [51.505, -0.09];
+  const mapZoom = 13;
+
   return (
     <div>
       <h1>Don</h1>
       {/* <div>
         <FirebaseSample />
       </div> */}
-      <div>
+      <div style={wrapper}>
         <ImageUpload />
       </div>
-      <div>
+      <div style={wrapper}>
         <SearchField />
+      </div>
+
+      <div style={wrapper}>
+        <MapSearch />
+      </div>
+      <div style={wrapper}>
+        <MapLeaflet />
       </div>
     </div>
   );
---
+++ src/components/module/MapLeaflet/MapLeaflet.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./mapLeaflet.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+const MapLeaflet = ({ center, zoom }) => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapLeaflet;
