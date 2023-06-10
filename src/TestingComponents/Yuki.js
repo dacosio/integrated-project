9574d4389363363a46e23c42d56f5ff21a42a07d
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-10 15:20:11 -0700

    [For Feature] feature/typography

--- src/TestingComponents/Yuki.js
+++ src/TestingComponents/Yuki.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typography from "../components/base/Typography/Typography";
+import SellingItemCard from "../components/base/SellingItemCard/SellingItemCard";
 
 const Yuki = () => {
   return (
@@ -18,12 +19,31 @@ const Yuki = () => {
         <Typography className="button-regular">button-regular</Typography>
         <Typography className="color-heading-body-grey">Color: Grey</Typography>
         <Typography className="color-heading-body-black">Color: Black</Typography>
+        <Typography className="color-primary-1">Color: primary-1</Typography>
+        <Typography className="color-primary-2">Color: primary-2</Typography>
+        <Typography className="color-primary-3">Color: primary-3</Typography>
+        <Typography className="color-primary-4">Color: primary-4</Typography>
+        <Typography className="color-accent-1">Color: accent-1</Typography>
+        <Typography className="color-accent-2">Color: accent-2</Typography>
+        <Typography className="color-accent-3">Color: accent-3</Typography>
+        <Typography className="color-accent-4">Color: accent-4</Typography>
         <div style={{ "background-color": "black" }}>
           <Typography className="color-heading-body-white">
             Color: white
           </Typography>
         </div>
         <Typography className="color-placeholder">placeholder: grey</Typography>
+
+        <h3>Selling Detail Card</h3>
+        <div>
+          <SellingItemCard
+            source="https://picsum.photos/200"
+            itemName="Item Name"
+            dateApproved="June 10, 2023"
+            quantity="1"
+            price="11"
+          />
+        </div>
         <hr />
       </section>
     </div>
