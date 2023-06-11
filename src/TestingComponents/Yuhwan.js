eba2f7e2c3d73e84cb7b0ec04ab13cb536a94227
yban <laubana@gmail.com>
2023-06-10 23:36:49 -0700

    my conponents added to testComponents

--- src/TestingComponents/Yuhwan.js
+++ src/TestingComponents/Yuhwan.js
@@ -2,21 +2,20 @@ import React, { useState } from "react";
 import Page from "../components/base/Page/Page";
 import Pagination from "../components/base/Pagination/Pagination";
 import SingleImageInput from "../components/base/SingleImageInput/SingleImageInput";
+import ImageInput from "../components/base/ImageInput/ImageInput";
+import NumberInput from "../components/base/NumberInput/NumberInput";
+import DatePicker from "../components/base/DatePicker/DatePicker";
+import TimePicker from "../components/base/TimePicker/TimePicker";
 
 const Yuhwan = (props) => {
+  /* Pagination */
   const [currentPageIndex, setCurrentPageIndex] = useState(1);
   const [pageNumber, setPageNumber] = useState(5);
   const [totalPageNumber, setTotalPageNumber] = useState();
   const [items, setItems] = useState([]);
 
-  const [singleImage, setSingleImage] = useState([]);
-
   const itemNumber = 10;
   const columns = ["id", "category", "title", "price"];
-
-  const SingleImageInputStyles = {
-    width: "30%",
-  };
 
   useState(() => {
     fetch(
@@ -65,6 +64,31 @@ const Yuhwan = (props) => {
       });
   };
 
+  /* SingleImageInput */
+  const [singleImage, setSingleImage] = useState([]);
+
+  const singleImageInputStyles = {
+    width: "10%",
+    margin: "auto",
+  };
+
+  /* MultipleImageInput */
+  const [multipleImages, setMultipleImages] = useState([]);
+
+  const multipleImageInputStyles = {
+    width: "30%",
+    margin: "auto",
+  };
+
+  /* NumberInput */
+  const [number, setNumber] = useState(0);
+
+  /* DatePicker */
+  const [date, setDate] = useState("");
+
+  /* TimePicker */
+  const [time, setTime] = useState("");
+
   return (
     <div>
       <h1>Yuhwan</h1>
@@ -89,12 +113,33 @@ const Yuhwan = (props) => {
           onClick={handleOnClick}
         />
       </div>
-      <div style={SingleImageInputStyles}>
+      <div>
         <h2>SingleImageInput</h2>
-        <SingleImageInput
-          images={singleImage}
-          setImages={setSingleImage}
-        ></SingleImageInput>
+        <div style={singleImageInputStyles}>
+          <SingleImageInput images={singleImage} setImages={setSingleImage} />
+        </div>
+      </div>
+      <div>
+        <h2>ImageInput</h2>
+        <div style={multipleImageInputStyles}>
+          <ImageInput images={multipleImages} setImages={setMultipleImages} />
+        </div>
+      </div>
+      <div>
+        <h2>NumberInput</h2>
+        <NumberInput
+          inputNumber={number}
+          setInputNumber={setNumber}
+          maxValue={10}
+        />
+      </div>
+      <div>
+        <h2>DatePicker</h2>
+        <DatePicker date={date} setDate={setDate} />
+      </div>
+      <div>
+        <h2>TimePicker</h2>
+        <TimePicker time={time} setTime={setTime} />
       </div>
     </div>
   );
