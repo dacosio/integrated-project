500a4b308994aad1b97aae2df30f068a75043229
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-10 22:06:25 -0700

    [For Review] feature/BuyerContactCard

--- src/components/base/BuyerContactCard/BuyerContactCard.jsx
+++ src/components/base/BuyerContactCard/BuyerContactCard.jsx
@@ -9,25 +9,28 @@ import EnvelopeSVG from "../SVG/PhoneSVG";
 const BuyerContactCard = ({ source, nameOfBuyer, contactTel, email }) => {
   return (
     <div className="buyer-contact-card">
-      <Typography className="h2-graphik-bold">Sold to</Typography>
-      <div className="image-container">
-        <Image source={source} />
+      <div className="title-container">
+        <Typography className="h2-graphik-bold">Sold to</Typography>
       </div>
-      <div className="buyer-contact-description">
-        <Typography className="name-of-buyer body-1-regular">
-          <UserSVG height={16} width={16} />
-          {nameOfBuyer}
-        </Typography>
+      <div className="buyer-description">
+        <div className="image-container">
+          <Image source={source} />
+        </div>
+        <div className="buyer-contact-description">
+          <div className="name-container">
+            <UserSVG height={16} width={16} />
+            <Typography className="body-1-regular">{nameOfBuyer}</Typography>
+          </div>
 
-        <Typography className="contact-tel body-1-regular">
-          <PhoneSVG height={16} width={16} />
-          {contactTel}
-        </Typography>
-
-        <Typography className="email body-1-regular">
-          <EnvelopeSVG height={16} width={16} />
-          {email}
-        </Typography>
+          <div className="conatct-tel-container">
+            <PhoneSVG height={16} width={16} />
+            <Typography className="body-1-regular">{contactTel}</Typography>
+          </div>
+          <div className="email-container">
+            <EnvelopeSVG height={16} width={16} />
+            <Typography className="body-1-regular">{email}</Typography>
+          </div>
+        </div>
       </div>
     </div>
   );
