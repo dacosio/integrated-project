3bc34325256bfb6bb166337ae24f34d2ba235823
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-10 21:44:01 -0700

    [For Review] feature/meetUpInfoCard

--- src/components/base/MeetUpInfoCard/MeetUpInfoCard.jsx
+++ src/components/base/MeetUpInfoCard/MeetUpInfoCard.jsx
@@ -8,23 +8,29 @@ import MapMarkerSVG from "../SVG/MapMarkerSVG";
 const MeetUpInfoCard = ({ date, time, location }) => {
   return (
     <div className="meet-up-info-card">
-      <Typography Class="h3-graphik-bold">Meet-up Info</Typography>
+      <div className="title-container">
+        <Typography Class="h3-graphik-bold">Meet-up Info</Typography>
+      </div>
       <div className="meet-up-info-description">
-        <Typography className="date body-1-regular">
+        <div className="date-container">
           <CalendarSVG height={16} width={16} />
-          {date}
-        </Typography>
+          <Typography className="body-1-regular">{date}</Typography>
+        </div>
 
-        <Typography className="time body-1-regular">
+        <div className="time-container">
           <ClockSVG height={16} width={16} />
-          {time}
-        </Typography>
+          <Typography className="body-1-regular">{time}</Typography>
+        </div>
 
-        <Typography className="location body-1-regular">
+        <div className="location-container">
           <MapMarkerSVG height={16} width={16} />
-          {location}
-        </Typography>
-        {/* <div style="max-width:100%; list-style:none; transition: none;overflow:hidden;width:500px;height:500px;"><div id="display-google-map" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Langara+College,+West+49th+Avenue,+Vancouver,+BC,+Canada&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="embedded-map-code" href="https://kbj9qpmy.com/hrn" id="get-data-for-map">InMotion Hosting</a><style>#display-google-map img{max-height:none;max-width:none!important;background:none!important;}</style></div> */}
+          <Typography className="location body-1-regular">
+            {location}
+          </Typography>
+        </div>
+        <div className="map-container">
+          {/* <div style="max-width:100%; list-style:none; transition: none;overflow:hidden;width:500px;height:500px;"><div id="display-google-map" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Langara+College,+West+49th+Avenue,+Vancouver,+BC,+Canada&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="embedded-map-code" href="https://kbj9qpmy.com/hrn" id="get-data-for-map">InMotion Hosting</a><style>#display-google-map img{max-height:none;max-width:none!important;background:none!important;}</style></div> */}
+        </div>
       </div>
     </div>
   );
