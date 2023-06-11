baf0ca87c4667380c2eaec764add8f39a5e1624d
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-10 21:11:37 -0700

    [For Review] feature/SellingItemCard

--- src/components/base/SellingItemCard/SellingItemCard.jsx
+++ src/components/base/SellingItemCard/SellingItemCard.jsx
@@ -12,24 +12,27 @@ const SellingItemCard = ({
 }) => {
   return (
     <div className="selling-item-card">
-        <div className="image-container">
-          <Image source={source} />
-        </div>
+      <div className="image-container">
+        <Image source={source} />
+      </div>
       <div className="selling-item-description">
         <Typography className="item-name color-primary-1 h3-graphik-bold">
           {itemName}
         </Typography>
-        <Typography className="h4-graphik-bold">Date approved: </Typography>
-        <Typography className="date-approved body-1-regular">
-          {dateApproved}
-        </Typography>
+        <div className="date-approved-container">
+          <Typography className="h4-graphik-bold">Date approved: </Typography>
+          <Typography className="date-approved body-1-regular">
+            {dateApproved}
+          </Typography>
+        </div>
 
-        <Typography className="body-1-regular">
-          <div class="quantity-title">Quantity:</div>
-          <div class="quantity">{quantity}</div>
-        </Typography>
-
-        <Typography className="price h1-graphik-bold">${price}</Typography>
+        <div className="quantity-container">
+          <Typography className="h4-graphik-bold">Quantity: </Typography>
+          <Typography className="body-1-regular">{quantity}</Typography>
+        </div>
+        <div className="price-container">
+          <Typography className="h1-graphik-bold">${price}</Typography>
+        </div>
       </div>
     </div>
   );
