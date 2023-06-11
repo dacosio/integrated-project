bf2a81f1496d35f885415752a1f3e175a7a7f95c
yban <laubana@gmail.com>
2023-06-10 23:03:19 -0700

    profile-detail page created

--- src/pages/Profile/ProfileDetail/ProfileDetail.container.js
+++ src/pages/Profile/ProfileDetail/ProfileDetail.container.js
@@ -3,7 +3,41 @@ import ProfileDetailView from "./ProfileDetail.view";
 
 const ProfileDetail = () => {
   const generatedProps = {
-    // generated props here
+    location: "Vancouver, British Columbia",
+    items: [
+      {
+        distance: 2.0,
+        days: 3,
+        source: "https://picsum.photos/500/500?random=1",
+        itemname: "Banana",
+        price: 10,
+        stock: 5,
+      },
+      {
+        distance: 2.0,
+        days: 3,
+        source: "https://picsum.photos/500/500?random=2",
+        itemname: "Banana",
+        price: 10,
+        stock: 5,
+      },
+      {
+        distance: 2.0,
+        days: 3,
+        source: "https://picsum.photos/500/500?random=3",
+        itemname: "Banana",
+        price: 10,
+        stock: 5,
+      },
+      {
+        distance: 2.0,
+        days: 3,
+        source: "https://picsum.photos/500/500?random=4",
+        itemname: "Banana",
+        price: 10,
+        stock: 5,
+      },
+    ],
   };
   return <ProfileDetailView {...generatedProps} />;
 };
--- src/pages/Profile/ProfileDetail/ProfileDetail.view.jsx
+++ src/pages/Profile/ProfileDetail/ProfileDetail.view.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import Imagewithdescription from "../../../components/base/ActiveListingCard/ActiveListingCard";
+import styles from "./profile-detail.module.css";
 
 const ProfileDetail = (props) => {
   return (
-    <div>
-      <h1>ProfileDetail Screen</h1>
+    <div className={`${styles["wrapper"]}`}>
+      <div>
+        <h1>Active Listings</h1>
+      </div>
+      <div className={`${styles["list"]}`}>
+        {props.items.map((item, index) => (
+          <Imagewithdescription
+            key={index}
+            distance={item.distance}
+            days={item.days}
+            source={item.source}
+            itemname={item.itemname}
+            price={item.price}
+            stock={item.stock}
+          />
+        ))}
+      </div>
     </div>
   );
 };
