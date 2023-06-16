2475f1e9bf221b1965018e5af90d0078a464cc8b
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-16 16:05:17 -0700

    [For Review] MeetUpInfo Card: jsx icon updated

--- src/components/base/MeetUpInfoCard/MeetUpInfoCard.jsx
+++ src/components/base/MeetUpInfoCard/MeetUpInfoCard.jsx
@@ -3,37 +3,42 @@ import Typography from "../Typography/Typography";
 import "./meetUpInfoCard.css";
 import CalendarSVG from "../SVG/CalendarSVG";
 import ClockSVG from "../SVG/ClockSVG";
-import MapMarkerSVG from "../SVG/MapMarkerSVG";
+import LocationSVG from "../SVG/LocationSVG";
+import MapLeaflet from "../../module/MapLeaflet/MapLeaflet";
 
 const MeetUpInfoCard = ({ date, time, location }) => {
   return (
-    <div className="meet-up-info-card">
-      <div className="title-container">
-        <Typography Class="h3-graphik-bold">Meet-up Info</Typography>
-      </div>
-      <div className="meet-up-info-description">
-        <div className="date-container">
-          <CalendarSVG height={16} width={16} />
-          <Typography className="body-1-regular">{date}</Typography>
+    <div className="meet-up-info-card-wrapper">
+      <div className="meet-up-info-card">
+        <div className="title-container">
+          <Typography className="h4-graphik-bold">Meet-up Info</Typography>
         </div>
-
-        <div className="time-container">
-          <ClockSVG height={16} width={16} />
-          <Typography className="body-1-regular">{time}</Typography>
-        </div>
-
-        <div className="location-container">
-          <MapMarkerSVG height={16} width={16} />
-          <Typography className="location body-1-regular">
-            {location}
-          </Typography>
-        </div>
-        <div className="map-container">
-          {/* <div style="max-width:100%; list-style:none; transition: none;overflow:hidden;width:500px;height:500px;"><div id="display-google-map" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Langara+College,+West+49th+Avenue,+Vancouver,+BC,+Canada&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="embedded-map-code" href="https://kbj9qpmy.com/hrn" id="get-data-for-map">InMotion Hosting</a><style>#display-google-map img{max-height:none;max-width:none!important;background:none!important;}</style></div> */}
+        <div className="meet-up-info-description">
+          <div className="date-container">
+            <CalendarSVG height={23} width={18} />
+            <Typography className="body-1-regular">{date}</Typography>
+          </div>
+          <div className="time-container">
+            <ClockSVG height={23} width={20} />
+            <Typography className="body-1-regular">{time}</Typography>
+          </div>
+          <div className="location-container">
+            <LocationSVG className="locationsvg" height={23} width={20} />
+            <Typography className="location body-1-regular">
+              {location}
+            </Typography>
+          </div>
+          <div className="map-container" style={{ "width": "100%", "height" : "auto"}}>
+            <MapLeaflet
+              center="49.225693, -123.107326"
+              direction="top"
+              permanent
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default MeetUpInfoCard;
+export default React.memo(MeetUpInfoCard);
