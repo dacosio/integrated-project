6211eeb670924f19f4e1f334a9e29858efb1fcfe
Don Apollo Cosio <dacosio@gmail.com>
2023-06-15 19:56:01 -0700

    firebase sample

--- src/TestingComponents/Don.js
+++ src/TestingComponents/Don.js
@@ -28,12 +28,9 @@ const Don = (props) => {
   return (
     <div>
       <h1>Don</h1>
-      {/* <div>
+      <div>
         <FirebaseSample />
-      </div> */}
-      {/* <div style={wrapper}>
-        <ImageUpload />
-      </div> */}
+      </div>
       <div style={wrapper}>
         <SearchField
           placeholder="What are you looking for?"
