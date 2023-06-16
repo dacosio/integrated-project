43940ca043ad0e0cd350d095340881f1e6b0b96b
yban <laubana@gmail.com>
2023-06-16 13:52:21 -0700

    logic changed

--- src/components/base/ImageList/ImageList.jsx
+++ src/components/base/ImageList/ImageList.jsx
@@ -3,39 +3,11 @@ import "react-toastify/dist/ReactToastify.css";
 import styles from "./image-list.module.css";
 
 function ImageList({ images, mode = "horizontal", ...props }) {
-
   return (
-    <>
-      {1 == images.length && (
-        <div className={`${styles["wrapper"]} ${styles[mode]}`}>
-          <div className={`${styles["header"]}`}>
-            <img className={`${styles["image"]} ${styles["main-image"]}`} src={images[0]} />
-          </div>
-        </div >)}
-      {1 < images.length && (
-        <div className={`${styles["wrapper"]} ${styles["grid"]} ${styles[mode]}`}>
-          <div className={`${styles["header"]}`}>
-            <img className={`${styles["image"]} ${styles["main-image"]}`} src={images[0]} />
-          </div>
-          {1 < images.length && images.length <= 3 && (
-            <div className={`${styles["footer"]}`}>
-              {images.slice(1, 3).map((image, index) => (
-                <img className={`${styles["image"]} ${styles["sub-image"]}`} src={image} key={index} />
-              ))}
-            </div>
-          )}
-          {3 < images.length && (
-            <div className={`${styles["footer"]}`}>
-              {images.slice(1, 3).map((image, index) => (
-                <img className={`${styles["image"]} ${styles["sub-image"]}`} src={image} key={index} />
-              ))}
-              <div className={`${styles["more"]}`}>+ {images.length - 3}</div>
-            </div>
-          )}
-        </div >
-      )
-      }
-    </>
+    <div className={`${styles["wrapper"]} ${styles[mode]}`} style={mode === "vertical" && 1 < images.length ? { gridTemplateColumns: `3fr repeat(1, 1fr)` } : null}>
+      {images.slice(0, 3).map((image, index) => (<img src={image} className={`${styles["item"]} ${styles["image"]}`} />))}
+      {3 < images.length && (<div className={`${styles["item"]}`}>+ {images.length - 3}</div>)}
+    </div>
   );
 }
 
