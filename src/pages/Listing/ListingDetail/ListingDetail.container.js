cac62637a704fdc4cebc8c65c4b4246a6dda6b9d
yban <laubana@gmail.com>
2023-06-16 18:25:18 -0700

    listing-detail page created

--- src/pages/Listing/ListingDetail/ListingDetail.container.js
+++ src/pages/Listing/ListingDetail/ListingDetail.container.js
@@ -2,8 +2,25 @@ import React from "react";
 import ListingDetailView from "./ListingDetail.view";
 
 const ListingDetail = () => {
+  const item = {
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur quam et urna tincidunt, aliquet sagittis odio tempor. Curabitur pellentesque interdum mauris pretium tincidunt.",
+    images: [
+      "https://picsum.photos/id/10/200/300",
+      "https://picsum.photos/id/20/200/300",
+      "https://picsum.photos/id/30/200/300",
+      "https://picsum.photos/id/40/200/300",
+      "https://picsum.photos/id/50/200/300",
+    ],
+    seller: {
+      name: "aishasells",
+      location: "Vancouver, British Columbia",
+    },
+  };
+
   const generatedProps = {
     // generated props here
+    item,
   };
   return <ListingDetailView {...generatedProps} />;
 };
--- src/pages/Listing/ListingDetail/ListingDetail.view.jsx
+++ src/pages/Listing/ListingDetail/ListingDetail.view.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import ImageList from "../../../components/base/ImageList/ImageList";
+import DescriptionCard from "../../../components/base/DescriptionCard/DescriptionCard";
+import SellerInfoCard from "../../../components/base/SellerInfoCard/SellerInfoCard";
+import useMediaQuery from "../../../utils/useMediaQuery";
+import styles from "./listing-detail.module.css";
 
 const ListingDetail = (props) => {
+  const { item } = props;
+
+  const isDesktop = useMediaQuery("(min-width: 1200px)");
+
   return (
-    <div>
+    <div className={`${styles["wrapper"]}`}>
       <h1>ListingDetail Screen</h1>
+      <ImageList images={item["images"]} mode="horizontal" />
+      <DescriptionCard description={item["description"]} />
+      <SellerInfoCard
+        username={item["seller"]["name"]}
+        location={item["seller"]["location"]}
+      />
     </div>
   );
 };
