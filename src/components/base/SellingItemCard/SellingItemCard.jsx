03ec0f94c125a60d0b5ce0f7704072de60f9d4cb
hchenghy <134082406+hchenghy@users.noreply.github.com>
2023-06-18 04:05:42 -0700

    [For Review]SellingItemCard: Typography Updated

--- src/components/base/SellingItemCard/SellingItemCard.jsx
+++ src/components/base/SellingItemCard/SellingItemCard.jsx
@@ -17,18 +17,18 @@ const SellingItemCard = ({
           <Image source={source} style={{ borderRadius: "10px" }} />
         </div>
         <div className="selling-item-description">
-          <Typography varient="item-name h3-graphik-bold">
+          <Typography variant="item-name h3-graphik-bold">
             {itemName}
           </Typography>
-          <Typography varient="h4-graphik-bold">Date approved: </Typography>
-          <Typography varient="date-approved body-1-regular">
+          <Typography variant="h4-graphik-bold">Date approved: </Typography>
+          <Typography variant="date-approved body-1-regular">
             {dateApproved}
           </Typography>
           <div className="quantity-container">
-            <Typography varient="h4-graphik-bold">Quantity: </Typography>
-            <Typography varient="body-1-regular">{quantity}</Typography>
+            <Typography variant="h4-graphik-bold">Quantity: </Typography>
+            <Typography variant="body-1-regular">{quantity}</Typography>
           </div>
-          <Typography varient="h1-graphik-bold">${price}</Typography>
+          <Typography variant="h1-graphik-bold">${price}</Typography>
         </div>
       </div>
     </div>
