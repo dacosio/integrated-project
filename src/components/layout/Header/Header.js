1df622f2ce8e4092941e4220403de326093b128d
Don Apollo Cosio <dacosio@gmail.com>
2023-06-20 01:37:49 -0700

    header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -5,17 +5,14 @@ import Button from "../../base/Button/Button";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchField from "../../base/SearchField/SearchField";
-import useWindowSize from "../../../utils/useWindowSize";
 import { SearchContext } from "../../../context/SearchContext";
 import { FilterSVG, LogoSVG } from "../../base/SVG";
 import useMediaQuery from "../../../utils/useMediaQuery";
-import Grid from "../Grid/Grid";
 
 const Header = () => {
   const { user, logout } = UserAuth();
   const [filterSelected, setFilterSelected] = useState(false);
   const navigate = useNavigate();
-  const [size] = useWindowSize();
 
   const handleLogOut = async () => {
     await logout();
@@ -31,9 +28,10 @@ const Header = () => {
 
   const sm = useMediaQuery("(min-width: 360px) and (max-width:576px)");
   const md = useMediaQuery("(min-width: 577px) and (max-width:799px)");
+  const lg = useMediaQuery("(min-width: 800px) and (max-width:1270px)");
   const xl = useMediaQuery("(min-width: 1271px");
 
-  let headerMobile = () => {
+  let headerSm = () => {
     return (
       <div className={styles.mobileWrapper}>
         <div className={styles.mobileTitle}>
@@ -60,6 +58,199 @@ const Header = () => {
             selected={filterSelected}
             onClick={() => setFilterSelected(!filterSelected)}
           />
+        </div>
+      </div>
+    );
+  };
+
+  let headerMd = () => {
+    return (
+      <div className={styles.mdWrapper}>
+        <div className={styles.mdTop}>
+          <div className={styles.logo} style={{ marginRight: "41px" }}>
+            <LogoSVG width={200} height={41} />
+          </div>
+          <div className={styles.mdRightButtons}>
+            <Button
+              variant="yellow"
+              hoverable
+              size="sm"
+              onClickHandler={() => navigate("listing/add")}
+              label="Post Listing"
+            />
+            <Button
+              variant="light-blue"
+              size="sm"
+              label="Logout"
+              hoverable
+              onClickHandler={handleLogOut}
+            />
+          </div>
+        </div>
+        <div className={styles.mdMid}>
+          <ul>
+            <li>
+              <Link
+                style={
+                  location.pathname === "/"
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
+                    : { color: "var(--white)" }
+                }
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={
+                  location.pathname === "/transaction"
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
+                    : { color: "var(--white)" }
+                }
+                to="transaction"
+              >
+                Orders
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={
+                  location.pathname === "/register"
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
+                    : { color: "var(--white)" }
+                }
+                to="register"
+              >
+                Settings
+              </Link>
+            </li>
+            <li>
+              <Link
+                style={
+                  location.pathname === "/user"
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
+                    : { color: "var(--white)" }
+                }
+                to="user"
+              >
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.mdBot}>
+          <SearchField
+            value={searchValue}
+            resetValue={resetSearchValue}
+            onChange={handleInputChange}
+            placeholder="What are you looking for?"
+          />
+          <FilterSVG
+            height={22}
+            width={25}
+            fill={"var(--white)"}
+            selected={filterSelected}
+            onClick={() => setFilterSelected(!filterSelected)}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  let headerLg = () => {
+    return (
+      <div className={styles.lgWrapper}>
+        <div className={styles.lgTop}>
+          <div className={styles.logo} style={{ marginRight: "41px" }}>
+            <LogoSVG width={200} height={41} />
+          </div>
+          <div className={styles.lgRightButtons}>
+            <Button
+              variant="yellow"
+              hoverable
+              size="sm"
+              onClickHandler={() => navigate("listing/add")}
+              label="Post Listing"
+            />
+            <Button
+              variant="light-blue"
+              size="sm"
+              label="Logout"
+              hoverable
+              onClickHandler={handleLogOut}
+            />
+          </div>
+        </div>
+        <div className={styles.lgBot}>
+          <div className={styles.lgNavLeft}>
+            <ul>
+              <li>
+                <Link
+                  style={
+                    location.pathname === "/"
+                      ? { color: "var(--yellow)", fontWeight: "bold" }
+                      : { color: "var(--white)" }
+                  }
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={
+                    location.pathname === "/transaction"
+                      ? { color: "var(--yellow)", fontWeight: "bold" }
+                      : { color: "var(--white)" }
+                  }
+                  to="transaction"
+                >
+                  Orders
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={
+                    location.pathname === "/register"
+                      ? { color: "var(--yellow)", fontWeight: "bold" }
+                      : { color: "var(--white)" }
+                  }
+                  to="register"
+                >
+                  Settings
+                </Link>
+              </li>
+              <li>
+                <Link
+                  style={
+                    location.pathname === "/user"
+                      ? { color: "var(--yellow)", fontWeight: "bold" }
+                      : { color: "var(--white)" }
+                  }
+                  to="user"
+                >
+                  Profile
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.lgFilterRight}>
+            <SearchField
+              value={searchValue}
+              resetValue={resetSearchValue}
+              onChange={handleInputChange}
+              placeholder="What are you looking for?"
+            />
+            <FilterSVG
+              height={22}
+              width={25}
+              fill={"var(--white)"}
+              selected={filterSelected}
+              onClick={() => setFilterSelected(!filterSelected)}
+            />
+          </div>
         </div>
       </div>
     );
@@ -93,7 +284,7 @@ const Header = () => {
               <Link
                 style={
                   location.pathname === "/"
-                    ? { color: "var(--yellow)" }
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
                     : { color: "var(--white)" }
                 }
                 to="/"
@@ -105,7 +296,7 @@ const Header = () => {
               <Link
                 style={
                   location.pathname === "/transaction"
-                    ? { color: "var(--yellow)" }
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
                     : { color: "var(--white)" }
                 }
                 to="transaction"
@@ -117,7 +308,7 @@ const Header = () => {
               <Link
                 style={
                   location.pathname === "/register"
-                    ? { color: "var(--yellow)" }
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
                     : { color: "var(--white)" }
                 }
                 to="register"
@@ -129,7 +320,7 @@ const Header = () => {
               <Link
                 style={
                   location.pathname === "/user"
-                    ? { color: "var(--yellow)" }
+                    ? { color: "var(--yellow)", fontWeight: "bold" }
                     : { color: "var(--white)" }
                 }
                 to="user"
@@ -162,16 +353,15 @@ const Header = () => {
       </div>
     );
   };
-  console.log(sm, xl);
-  let content = sm ? (
-    headerMobile()
-  ) : md ? (
-    <div>nill</div>
-  ) : xl ? (
-    headerXl()
-  ) : (
-    ""
-  );
+  let content = sm
+    ? headerSm()
+    : md
+    ? headerMd()
+    : lg
+    ? headerLg()
+    : xl
+    ? headerXl()
+    : headerXl();
 
   return <>{content}</>;
 };
