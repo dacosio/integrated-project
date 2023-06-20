3175300185868271cde7586265f8968a52bac904
yban <laubana@gmail.com>
2023-06-19 21:07:32 -0700

    typography added, transition-duration added

--- src/components/base/Accordion/Accordion.jsx
+++ src/components/base/Accordion/Accordion.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
+import Typography from "../Typography/Typography";
 import styles from "./accordion.module.css";
 
 const Accordion = ({ children, visibility, onToggle, label, ...props }) => {
+  console.log(visibility);
   return (
     <>
       <div className={`${styles["wrapper"]}`}>
         <div className={`${styles["header"]}`} onClick={onToggle}>
-          <div>{label}</div>
+          <Typography variant="h4-graphik-bold">{label}</Typography>
           {visibility ? (
             <MdKeyboardArrowUp size={24} />
           ) : (
             <MdKeyboardArrowDown size={24} />
           )}
         </div>
-        {visibility && <div className={`${styles["footer"]}`}>{children}</div>}
+        <div
+          className={`${styles["footer"]} ${
+            visibility ? styles["expand"] : ""
+          }`}
+        >
+          <div style={{ padding: "0 16px 16px 16px" }}>{children}</div>
+        </div>
       </div>
     </>
   );
