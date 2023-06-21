a4e1f2fed97c239a83ce11fa9bf6f1953b4714ca
yban <laubana@gmail.com>
2023-06-20 17:51:57 -0700

    created

--- src/pages/Listing/ListingDetail/ListingDetail.container.js
+++ src/pages/Listing/ListingDetail/ListingDetail.container.js
@@ -1,11 +1,84 @@
-import React from "react";
-import ListingDetailView from "./ListingDetail.view";
+import React, { useState, useEffect } from "react";
+import store, { storage } from "../../../config/firebaseConfig";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+  query,
+  orderBy,
+  startAfter,
+  limit,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from "firebase/firestore";
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+import ListDetailView from "./ListingDetail.view";
 
 const ListingDetail = () => {
+  const [product, setProduct] = useState({});
+  const [user, setUser] = useState({});
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const productRef = doc(store, "product", "LiI8AlOLDOEusml6Eg9K");
+
+    getDoc(productRef)
+      .then((productResponse) => {
+        setProduct(productResponse.data());
+
+        const userRef = doc(
+          store,
+          "user",
+          productResponse.data()["created_by"]["id"]
+        );
+
+        getDoc(userRef)
+          .then((userResponse) => {
+            setUser(userResponse.data());
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const directoryRef = ref(storage, "image");
+
+    listAll(directoryRef)
+      .then(async (res) => {
+        const files = [];
+
+        const promises = res.items.map(async (fileRef, index) => {
+          await getDownloadURL(fileRef).then((url) => {
+            files.push(url);
+          });
+        });
+
+        await Promise.all(promises);
+
+        setImages([...files.sort()]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   const generatedProps = {
+    product: product,
+    user: user,
+    images: images,
     // generated props here
   };
-  return <ListingDetailView {...generatedProps} />;
+  return <ListDetailView {...generatedProps} />;
 };
 
 export default ListingDetail;
--- src/pages/Listing/ListingDetail/ListingDetail.view.jsx
+++ src/pages/Listing/ListingDetail/ListingDetail.view.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import useMediaQuery from "../../../utils/useMediaQuery";
+import ImageList from "../../../components/base/ImageList/ImageList";
+import DescriptionCard from "../../../components/base/DescriptionCard/DescriptionCard";
+import styles from "./listing-detail.module.css";
+import Typography from "../../../components/base/Typography/Typography";
+import SellerInfoCard from "../../../components/base/SellerInfoCard/SellerInfoCard";
 
 const ListingDetail = (props) => {
-  return (
-    <div>
-      <h1>ListingDetail Screen</h1>
-    </div>
-  );
+  const { product, user, images } = props;
+
+  const isDesktop = useMediaQuery("(min-width: 1200px)");
+  const isTablet = useMediaQuery("(min-width: 1200px)");
+  const isMobile = useMediaQuery("(min-width: 360px)");
+
+  if (isDesktop) {
+    return (
+      <div className={`${styles["wrapper"]}`}>
+        <div className={`${styles["card"]}`}>
+          <ImageList images={images} />
+        </div>
+        <DescriptionCard description={product.description} />
+      </div>
+    );
+  } else {
+    return (
+      <div className={`${styles["wrapper"]}`}>
+        <div className={`${styles["card"]}`}>
+          <Typography variant="h3-graphik-bold" color="dark-blue">
+            {product.name}
+          </Typography>
+          <ImageList images={images} />
+        </div>
+        <DescriptionCard description={product.description} />
+        <SellerInfoCard
+          username={user.username}
+          location={`${user.city}, ${user.state}`}
+        />
+      </div>
+    );
+  }
 };
 
 export default ListingDetail;
