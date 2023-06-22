d3fefe89c8881e66d29cc4eca65c1b34af71467c
yban <laubana@gmail.com>
2023-06-21 19:02:50 -0700

    modal created

---
+++ src/components/base/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { GrClose } from "react-icons/gr";
+import styles from "./modal.module.css";
+
+const Modal = ({
+  top = "25vh",
+  width = "50vw",
+  hasBackground = true,
+  visibility,
+  onClose,
+  children,
+  ...props
+}) => {
+  return (
+    <>
+      {visibility && (
+        <div className={`${styles["outer-background"]}`}>
+          <div
+            className={`${styles.wrapper} ${
+              hasBackground ? styles["inner-background"] : null
+            }`}
+            style={{ width: width }}
+          >
+            <div className={`${styles["btn-wrapper"]} `}>
+              <GrClose
+                size={24}
+                className={`${styles.btn}`}
+                onClick={onClose}
+              />
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default React.memo(Modal);
