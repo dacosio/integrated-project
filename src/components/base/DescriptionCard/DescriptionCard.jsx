62ad9908639c665024fb0e8c425860c935acedc9
Yuki Cheng <hcheng40@mylangara.ca>
2023-06-25 01:11:18 -0700

    [For Review] DescriptionCard: re-push

--- src/components/base/DescriptionCard/DescriptionCard.jsx
+++ src/components/base/DescriptionCard/DescriptionCard.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import "./DescriptionCard.css";
 import Button from "./../Button/Button";
 import Typography from "../Typography/Typography";
-// import { EvalDevToolModulePlugin } from "webpack";
 
 const DescriptionCard = ({ description, onClick }) => {
   return (
     <div className="dcwrapper">
       <div className="description-container">
         <div className="item-description">
-          <Typography variant="h4-graphik-bold">Description</Typography>
-          <Typography variant="body-2-regular">{description}</Typography>
+          <Typography variant="h4-graphik-bold" style={{ margin: "8px 0" }}>
+            Description
+          </Typography>
+          <Typography variant="body-2-regular" color="gray">
+            {description}
+          </Typography>
         </div>
-        {/* <div className="portion-description">
-        <h2>Each portion contains</h2>
-        <p>{portionDescription}</p>
-        </div> */}
         <div className="button">
           <Button
             onClickHandler={onClick}
             size="lg"
-            variant="primary"
+            variant="yellow"
             label="Request Purchase"
           />
         </div>
