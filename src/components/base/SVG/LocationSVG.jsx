6eea02b7f0b98eb1980b4d892bd6c7a6528ac458
hchenghy <134082406+hchenghy@users.noreply.github.com>
2023-06-25 04:09:04 -0700

    [For Review] LocationSVG Update

--- src/components/base/SVG/LocationSVG.jsx
+++ src/components/base/SVG/LocationSVG.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const HomeSVG = ({ width, height, fill, stroke, ...props }) => {
+const LocationSVG = ({ width, height, fill, stroke, ...props }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,4 +17,4 @@ const HomeSVG = ({ width, height, fill, stroke, ...props }) => {
   );
 };
 
-export default HomeSVG;
+export default LocationSVG;
