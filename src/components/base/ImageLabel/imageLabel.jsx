39fb5c2e871bc154252cb8f469e6557c001113d7
Don Apollo Cosio <dacosio@gmail.com>
2023-06-27 14:55:06 -0700

    removed

--- src/components/base/ImageLabel/imageLabel.jsx
+++ src/components/base/ImageLabel/imageLabel.jsx
@@ -12,19 +12,8 @@ export const imageLabel = (props) => {
             <Typography variant="body-4-regular" color="dark-blue">
               {distance == 1 ? distance + " km" : distance + " kms"}
             </Typography>
-            <div
-              style={{
-                borderRight: "2px solid var(--dark-blue)",
-                paddingLeft: "5px",
-                marginRight: "5px",
-              }}
-            ></div>
           </>
         )}
-
-        <Typography variant="body-4-regular" color="dark-blue">
-          {days == 1 ? days + " day ago" : days + " days ago"}
-        </Typography>
       </div>
     </div>
   );
