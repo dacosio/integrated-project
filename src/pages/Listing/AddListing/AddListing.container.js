137c9a013150e27b3b27a3dcd866dd5daccc6317
yban <laubana@gmail.com>
2023-07-16 00:43:06 -0700

    navigate changed after creating a listing

--- src/pages/Listing/AddListing/AddListing.container.js
+++ src/pages/Listing/AddListing/AddListing.container.js
@@ -123,7 +123,28 @@ const AddListing = () => {
             updateDoc(doc(db, "product", productResponse.id), {
               images: __images,
             }).then((updateResponse) => {
-              navigate(`/listing/${productResponse.id}`);
+              getDoc(doc(db, "product", productResponse.id)).then(
+                (_productResponse) => {
+                  navigate(`/listing/${_productResponse.id}`, {
+                    state: {
+                      id: _productResponse.id,
+                      createdAt: _productResponse.data().createdAt,
+                      createdByDisplayName:
+                        _productResponse.data().createdByNickName,
+                      createdByIdent: _productResponse.data().createdByIdent,
+                      description: _productResponse.data().description,
+                      images: _productResponse.data().images,
+                      latitude: _productResponse.data().lat,
+                      longitude: _productResponse.data().long,
+                      meetUpAddress: _productResponse.data().meetUpAddress,
+                      meetUpInfo: _productResponse.data().meetUpInfo,
+                      name: _productResponse.data().name,
+                      price: _productResponse.data().price,
+                      qty: _productResponse.data().qty,
+                    },
+                  });
+                }
+              );
             });
           });
         });
