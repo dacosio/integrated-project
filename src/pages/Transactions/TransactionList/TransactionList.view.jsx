b846451c0ab25b84570dc341e8981730bcdcf8f2
Don Apollo Cosio <dacosio@gmail.com>
2023-07-18 12:39:25 -0700

    state;

--- src/pages/Transactions/TransactionList/TransactionList.view.jsx
+++ src/pages/Transactions/TransactionList/TransactionList.view.jsx
@@ -58,54 +58,53 @@ const TransactionList = (props) => {
             today.setHours(0, 0, 0, 0);
             const timeDiff = today.getTime() - updatedAt.getTime();
             const days = Math.abs(Math.floor(timeDiff / (1000 * 60 * 60 * 24)));
-            console.log(o);
             return (
-              <Link
-                to={`/transaction/${o.id}`}
-                state={{
-                  id: o.id,
-                  createdAt: o.createdAt,
-                  splitteeName: o.splitteeName,
-                  splitterName: o.splitterName,
-                  imageUrl: o.imageUrl,
-                  latitude: o.latitude,
-                  longitude: o.longitude,
-                  meetUpAddress: o.meetupAddress,
-                  meetUpInfo: o.meetupSchedule,
-                  name: o.name,
-                  price: o.price,
-                  qty: o.qty,
-                  splitteeId: o.splitteeId,
-                  splitteeContactNumber: o.splitteeContactNumber,
-                  splitteeEmail: o.splitteeEmail,
-                  splitteeImageUrl: o.splitteeImageUrl,
-                  splitterContactNumber: o.splitteeContactNumber,
-                  splitterEmail: o.splitterEmail,
-                  splitterId: o.splitterId,
-                  splitterImageUrl: o.splitterImageUrl,
-                  orderStatus: o.orderStatus,
-                  productId: o.productId,
+              <TransactionCard
+                key={o.id}
+                type={o.splitteeId === user.uid ? "buying" : "selling"}
+                itemName={o.name}
+                time={days}
+                orderType={orderType}
+                portions={o.qty}
+                splitterName={
+                  orderType === "selling" ? o.splitteeName : o.splitterName
+                }
+                price={o.price}
+                source={o.imageUrl}
+                orderStatus={o.orderStatus}
+                onCancel={() => onCancel(o.id, o.productId)}
+                onDecline={() => onDecline(o.id, o.productId)}
+                onAccept={() => onAccept(o.id, o.productId)}
+                onComplete={() => onComplete(o.id, o.productId)}
+                onClick={() => {
+                  navigate(`/transaction/${o.id}`, {
+                    state: {
+                      id: o.id,
+                      createdAt: o.createdAt,
+                      splitteeName: o.splitteeName,
+                      splitterName: o.splitterName,
+                      imageUrl: o.imageUrl,
+                      latitude: o.latitude,
+                      longitude: o.longitude,
+                      meetUpAddress: o.meetupAddress,
+                      meetUpInfo: o.meetupSchedule,
+                      name: o.name,
+                      price: o.price,
+                      qty: o.qty,
+                      splitteeId: o.splitteeId,
+                      splitteeContactNumber: o.splitteeContactNumber,
+                      splitteeEmail: o.splitteeEmail,
+                      splitteeImageUrl: o.splitteeImageUrl,
+                      splitterContactNumber: o.splitteeContactNumber,
+                      splitterEmail: o.splitterEmail,
+                      splitterId: o.splitterId,
+                      splitterImageUrl: o.splitterImageUrl,
+                      orderStatus: o.orderStatus,
+                      productId: o.productId,
+                    },
+                  });
                 }}
-              >
-                <TransactionCard
-                  key={o.id}
-                  type={o.splitteeId === user.uid ? "buying" : "selling"}
-                  itemName={o.name}
-                  time={days}
-                  orderType={orderType}
-                  portions={o.qty}
-                  splitterName={
-                    orderType === "selling" ? o.splitteeName : o.splitterName
-                  }
-                  price={o.price}
-                  source={o.imageUrl}
-                  orderStatus={o.orderStatus}
-                  onCancel={() => onCancel(o.id, o.productId)}
-                  onDecline={() => onDecline(o.id, o.productId)}
-                  onAccept={() => onAccept(o.id, o.productId)}
-                  onComplete={() => onComplete(o.id, o.productId)}
-                />
-              </Link>
+              />
             );
           })}
         </Grid>
