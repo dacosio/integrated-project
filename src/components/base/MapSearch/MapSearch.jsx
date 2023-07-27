5b171d1ef6ccf2f55af2fb9d88bdf761ee1be27c
yban <laubana@gmail.com>
2023-07-26 18:52:33 -0700

    post listing map bug fix

--- src/components/base/MapSearch/MapSearch.jsx
+++ src/components/base/MapSearch/MapSearch.jsx
@@ -43,6 +43,7 @@ const MapSearch = ({
   };
 
   const handleSelectPlace = (item) => {
+    updatePlaceValue();
     getPlacePredictions({ input: "" });
     setValue(item.description);
     placesService?.getDetails(
